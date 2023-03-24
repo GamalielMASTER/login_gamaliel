136dc54e1f8817f1e5211c20597a5eddacb0a6d1
GamalielMASTER <dual.leon.solis@gmail.com>
2023-03-24 01:58:54 -0500

    reando para registrase

--- app/src/main/java/com/example/logingama/MainActivity.kt
+++ app/src/main/java/com/example/logingama/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.logingama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var authStateListener : FirebaseAuth.AuthStateListener
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +20,40 @@ class MainActivity : AppCompatActivity() {
         val btningresar: Button = findViewById(R.id.btnIngresar)
         val txtEmail: TextView = findViewById(R.id.edtEmail)
         val txtpass: TextView = findViewById(R.id.edtPassword)
+        val btnCrear_CuentaNueva : TextView = findViewById(R.id.btnCrearCuentaNueva)
+        firebaseAuth= Firebase.auth
         btningresar.setOnClickListener()
         {
-
+            signIn(txtEmail.text.toString(),txtpass.text.toString())
+        }
+        btnCrear_CuentaNueva.setOnClickListener()
+        {
+            val i = Intent (this, CrearCuentaActivity::class.java)
+            startActivity(i)
         }
 
     }
-    private fun signIn(email: String, password: String )
-    {
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this ) { task ->
-                if (task.isSuccessful)
-                    val user = firebaseAuth.currentUser
-                    Toast.makeText(baseContext,user?.uid.toString(), Toast.LENGTH_SHORT).show()
-                
 
+    private fun signIn(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext, "exitoso", Toast.LENGTH_SHORT).show()
+                    val i = Intent(this, MainActivity2::class.java )
+                    startActivity(i)
+                }
+                else {
+                    Toast.makeText(baseContext, "error de email y/o contraseña", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
+    }
+
 }
+
+
+
+
+
+
