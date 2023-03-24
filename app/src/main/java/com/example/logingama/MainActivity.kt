705c4edd8e6f70fc4dcfe07685e4cd1e0ed40bb9
GamalielMASTER <dual.leon.solis@gmail.com>
2023-03-24 00:32:19 -0500

    programado la actividad principal

--- app/src/main/java/com/example/logingama/MainActivity.kt
+++ app/src/main/java/com/example/logingama/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.logingama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener : FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btningresar: Button = findViewById(R.id.btnIngresar)
+        val txtEmail: TextView = findViewById(R.id.edtEmail)
+        val txtpass: TextView = findViewById(R.id.edtPassword)
+        btningresar.setOnClickListener()
+        {
+
+        }
+
     }
+    private fun signIn(email: String, password: String )
+    {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this ) { task ->
+                if (task.isSuccessful)
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext,user?.uid.toString(), Toast.LENGTH_SHORT).show()
+                
+
+            }
 }
