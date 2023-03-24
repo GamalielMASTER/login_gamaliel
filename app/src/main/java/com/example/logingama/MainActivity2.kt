d1493b4679e6af859df838d259d23b973178d076
GamalielMASTER <dual.leon.solis@gmail.com>
2023-03-24 14:17:29 -0500

    creando activity 3

--- app/src/main/java/com/example/logingama/MainActivity2.kt
+++ app/src/main/java/com/example/logingama/MainActivity2.kt
@@ -2,10 +2,19 @@ package com.example.logingama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
+
+
+    //override fun onCreateOptionsMenu(manu: Menu?): Boolean {
+        //menuInflater.inflate(R.menu.nav_menu,Menu!)
+        //return true
+
+   // }
 }
